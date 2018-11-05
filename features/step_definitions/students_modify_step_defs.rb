7fceb319e332362ad55824358c566d3d5d0937b5
tech-a49 <tom.e.beckett@gmail.com>
2018-11-05 12:28:08 +0000

    conflict resolution

--- features/step_definitions/students_modify_step_defs.rb
+++ features/step_definitions/students_modify_step_defs.rb
@@ -1,9 +1,5 @@
-Given("I am on a page") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
 When("I click on the add student link") do
-    pending # Write code here that turns the phrase above into concrete actions
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then("I should be taken the add student form") do
