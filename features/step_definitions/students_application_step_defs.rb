49f783dde6a786bde604409cf1f52d0bb0ba716f
Arun Kalwan <aruns1@hotmail.co.uk>
2018-11-05 14:28:59 +0000

    First Scenario tested

--- features/step_definitions/students_application_step_defs.rb
+++ features/step_definitions/students_application_step_defs.rb
@@ -1,36 +1,33 @@
 Given("I am on the students page OR a groups page") do
   student_page.visit_students
   sleep 1
-  student_page.click_student "Dan"
-  sleep 1
-  student_page.visit_students
-  sleep 1
-  student_page.select_new_student
-  sleep 2
 end
 
 When("I click on a student") do
-  pending # Write code here that turns the phrase above into concrete actions
+  student_page.click_student "Dan"
+  sleep 1
 end
 
 Then("I should be taken to the individual student page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(student_page.is_profile).to be true
 end
 
 Then("I should be presented with information on the student") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(student_page.student_information).to be true
 end
 
 Given("I am on a page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  student_page.visit_homepage
+  sleep 1
 end
 
 When("I click the students link") do
-  pending # Write code here that turns the phrase above into concrete actions
+  student_page.visit_students
+  sleep 1
 end
 
 Then("I should be taken to the students page") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(student_page.current_url).to eq 'http://localhost:3000/students'
 end
 
 Then("I should see a list of students") do
